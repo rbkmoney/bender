3daf3f4063dc64243015dbda0a91b3cd8e9c7efb
Sergei Shuvatov <s.shuvatov@rbkmoney.com>
2019-04-02 18:15:34 +0300

    CAPI-344: test with more recent version of machinegun

--- docker-compose.sh
+++ docker-compose.sh
@@ -15,7 +15,7 @@ services:
         condition: service_healthy
 
   machinegun:
-    image: dr.rbkmoney.com/rbkmoney/machinegun:1844dff663c24acdcd32f30ae3ea208f5d05a008
+    image: dr.rbkmoney.com/rbkmoney/machinegun:5e26162266a3bcf857852cb7844e5626fb0ebf7a
     command: /opt/machinegun/bin/machinegun foreground
     volumes:
       - ./test/machinegun/config.yaml:/opt/machinegun/etc/config.yaml
