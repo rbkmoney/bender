f19b8416f2bf772c21d5152da599b87336376059
Sergey Yelin <elinsn@gmail.com>
2021-09-02 10:50:15 +0300

    Format

--- apps/bender/test/generator_tests_SUITE.erl
+++ apps/bender/test/generator_tests_SUITE.erl
@@ -45,9 +45,9 @@ groups() ->
 init_per_suite(C) ->
     Apps =
         genlib_app:start_application(how_are_you) ++
-        genlib_app:start_application_with(scoper, [
-            {storage, scoper_storage_logger}
-        ]) ++
+            genlib_app:start_application_with(scoper, [
+                {storage, scoper_storage_logger}
+            ]) ++
             genlib_app:start_application_with(bender, [
                 {sequence, #{
                     path => <<"/v1/stateproc/bender_sequence">>,
